d2d6e2e5a19f7b82b1cd0cd6736876fea64b8e94
Ryoya Momose <25890337+rn0rno@users.noreply.github.com>
2017-12-29 21:27:01 +0900

    Update e-typing.js

--- js/e-typing.js
+++ js/e-typing.js
@@ -1,15 +1,19 @@
+txt={};
 $('#typing_content').contents().find('#current .result_data ul li').each(function(i){
-title=$(this).find('.title').text();
-data=$(this).find('.data').text();
-txt[title]=data;
+	title=$(this).find('.title').text();
+	data=$(this).find('.data').text();
+	txt[title]=data;
 });
 
 if($('#results_table').length){
-d=$('#results_table').val(JSON.stringify(txt));
-d.attr('style','width:80%;');
-}else{
-d=$('<input>').val(JSON.stringify(txt))
-d.attr('id','results_table');
-d.attr('style','width:80%;');
-$('.pp_description').html(d);
+	d=$('#results_table').val(JSON.stringify(txt));
+	d.attr('style','width:80%;');
+} else {
+	d=$('<input>').val(JSON.stringify(txt))
+	d.attr('id','results_table');
+	d.attr('style','width:80%;');
+	$('.pp_description').html(d);
 }
+
+$('#results_table').select();
+document.execCommand('copy');
