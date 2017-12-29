c3a2bfd42566cb72ae5f373191120475daea19e4
Ryoya Momose <25890337+rn0rno@users.noreply.github.com>
2017-12-29 21:13:21 +0900

    Create e-typing.js

---
+++ js/e-typing.js
@@ -0,0 +1,15 @@
+$('#typing_content').contents().find('#current .result_data ul li').each(function(i){
+title=$(this).find('.title').text();
+data=$(this).find('.data').text();
+txt[title]=data;
+});
+
+if($('#results_table').length){
+d=$('#results_table').val(JSON.stringify(txt));
+d.attr('style','width:80%;');
+}else{
+d=$('<input>').val(JSON.stringify(txt))
+d.attr('id','results_table');
+d.attr('style','width:80%;');
+$('.pp_description').html(d);
+}
