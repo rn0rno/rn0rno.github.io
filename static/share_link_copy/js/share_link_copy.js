e62cb741b6f5b532c0badae5a8ea803fae51e7c0
Momose Ryoya <ryoya.momose@togawa.cs.waseda.ac.jp>
2018-03-27 16:44:20 +0900

    リファクタ

--- static/share_link_copy/js/share_link_copy.js
+++ static/share_link_copy/js/share_link_copy.js
@@ -1,16 +1,8 @@
 var select_text = window.getSelection().toString()
 
-var title_name = document.getElementsByTagName('title')[0]
-
-if (!title_name){
-  title_text = ''
-} else {
-  title_text = title_name.text + ' '
-}
-
 if (select_text != ""){
   select_text += " | "
 }
 
-var share_text = select_text + title_text + location.href
+var share_text = select_text + document.title + ' ' + location.href
 window.prompt("", share_text);
