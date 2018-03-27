3316742194739d4c09eab9615f7f84d12a9fef13
Momose Ryoya <ryoya.momose@togawa.cs.waseda.ac.jp>
2018-03-27 16:29:39 +0900

    fix variable name

--- static/share_link_copy/js/share_link_copy.js
+++ static/share_link_copy/js/share_link_copy.js
@@ -1,3 +1,3 @@
-var default = '| ' + document.getElementsByTagName('title')[0].text + ' - ' + location.href
-console.log(default)
-user = window.prompt("コメントがあれば入力してください", default);
+var share_text = '| ' + document.getElementsByTagName('title')[0].text + ' - ' + location.href
+console.log(share_text)
+user = window.prompt("コメントがあれば入力してください", share_text);
