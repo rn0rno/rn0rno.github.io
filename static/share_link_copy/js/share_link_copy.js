b22049cd4873ee7fd62b79bd2b709843b564f95f
Momose Ryoya <ryoya.momose@togawa.cs.waseda.ac.jp>
2018-03-27 16:28:43 +0900

    add function

--- static/share_link_copy/js/share_link_copy.js
+++ static/share_link_copy/js/share_link_copy.js
@@ -1,1 +1,3 @@
-console.log('aaa')
+var default = '| ' + document.getElementsByTagName('title')[0].text + ' - ' + location.href
+console.log(default)
+user = window.prompt("コメントがあれば入力してください", default);
