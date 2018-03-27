1ca7711208199fa38ac185594ccd38b051c30c5f
Momose Ryoya <ryoya.momose@togawa.cs.waseda.ac.jp>
2018-03-27 16:30:53 +0900

    調整

--- static/share_link_copy/js/share_link_copy.js
+++ static/share_link_copy/js/share_link_copy.js
@@ -1,3 +1,3 @@
-var share_text = '| ' + document.getElementsByTagName('title')[0].text + ' - ' + location.href
+var share_text = ' | ' + document.getElementsByTagName('title')[0].text + ' - ' + location.href
 console.log(share_text)
 user = window.prompt("コメントがあれば入力してください", share_text);
