7d2c54a69552430f4dd745fa19aa5b2648bcde74
Momose Ryoya <ryoya.momose@togawa.cs.waseda.ac.jp>
2018-03-27 16:39:45 +0900

    選択したテキストを盛り込む

--- static/share_link_copy/js/share_link_copy.js
+++ static/share_link_copy/js/share_link_copy.js
@@ -1,11 +1,16 @@
+var select_text = window.getSelection().toString()
+
 var title_name = document.getElementsByTagName('title')[0]
 
 if (!title_name){
   title_text = ''
 } else {
-  title_text = title_name.text + ' - '
+  title_text = title_name.text + ' '
 }
 
-var share_text = ' | ' +  title_text + location.href
-console.log(share_text)
-user = window.prompt("コメントがあれば入力してください", share_text);
+if (select_text != ""){
+  select_text += " | "
+}
+
+var share_text = select_text + title_text + location.href
+window.prompt("", share_text);
