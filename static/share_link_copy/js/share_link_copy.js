fad74c027759ca18733864ad4a277722a16e40be
Momose Ryoya <ryoya.momose@togawa.cs.waseda.ac.jp>
2018-03-30 18:59:37 +0900

    いい感じに修正

--- static/share_link_copy/js/share_link_copy.js
+++ static/share_link_copy/js/share_link_copy.js
@@ -1,8 +1,7 @@
 var select_text = window.getSelection().toString()
 
 if (select_text != ""){
-  select_text += " | "
+  select_text += " / "
 }
 
-var share_text = select_text + document.title + ' ' + location.href
-window.prompt("", share_text);
+window.prompt("", select_text + '"' + document.title + '" ' + document.URL);
